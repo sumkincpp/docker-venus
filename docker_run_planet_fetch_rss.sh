69013918beb78c11d860f2b02ba7677f55f15422
Fedor Sumkin <qosys.net@gmail.com>
2014-11-09 20:52:53 +0300

    custom config for fetching

--- docker_run_planet_fetch_rss.sh
+++ docker_run_planet_fetch_rss.sh
@@ -1,1 +1,12 @@
-docker run -v `pwd`/data:/opt/planet docker-venus planet --verbose planet.ini
+#!/bin/sh
+
+config=planet.ini
+
+if [ ! -z "$1" ] 
+then
+  config=$1
+fi
+
+echo Generating Planet Venus with $config
+
+docker run -v `pwd`/data:/opt/planet docker-venus planet --verbose "$config"
